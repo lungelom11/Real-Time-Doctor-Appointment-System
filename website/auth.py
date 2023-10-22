2f1827eeafcb7d4974c6584ded4d08293e7c540d
Lungelo <Lungelomkhwemte11@gmail.com>
2023-10-22 20:32:35 +0200

    added register, login and logout code

---
+++ website/auth.py
@@ -0,0 +1,65 @@
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
+auth = Blueprint("auth", __name__)
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method =="POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = User.query.filter_by(email=email).first()
+        
+        if user:
+            if check_password_hash(user.password, password):
+                    return redirect(url_for("views.admin"))
+                    flash("Logged in successfully", category="success")    
+            else:
+                flash("Incorrect Password, please try again", category="error")
+        else:
+            flash("Email does not exist!", category="error")
+
+    return render_template("login.html")
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        firstname = request.form.get("firstname")
+        lastname = request.form.get("lastname")
+        email = request.form.get("email")
+        password = request.form.get("password")
+        password2 = request.form.get("confirm-password")
+        role = request.form.get("role")
+
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash("Email already exists!", category="error")
+
+        elif len(email) < 4:
+            flash("Email is too short.", category="error")
+
+        elif len(password) < 5:
+            flash("Password too short, must be at least 6 characters.", category="error")
+
+        elif password != password2:
+            flash("Passwords do not match, please enter again.", category="error")
+
+        else:
+            new_user = User(email=email, firstname=firstname, lastname=lastname, password=generate_password_hash(password), role=role)
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Account created!", category="success")
+            return redirect(url_for("views.admin"))
+            
+            
+
+    return render_template("register.html")
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("auth.login"))
---
+++ website/views.py
@@ -0,0 +1,51 @@
+from flask import Blueprint, render_template, request,redirect, url_for,flash
+from flask_login import login_required, current_user
+from .models import User
+from . import db
+from werkzeug.security import generate_password_hash
+
+views = Blueprint("views", __name__)
+
+@views.route("/")
+def home():
+    return render_template("home.html")
+
+@views.route("/admin")
+def admin():
+    users = User.query.all()
+    return render_template("admin.html", users = users)
+
+
+@views.route("/<int:id>/update", methods=["GET","POST"])
+def update(id):
+    update_user = User.query.filter_by(id = id).first()
+    if request.method == "POST":
+        if update_user:
+            db.session.delete(update_user)
+            db.session.commit()
+            
+            firstname = request.form.get("firstname")
+            lastname = request.form.get("lastname")
+            email = request.form.get("email")
+            password = request.form.get("password")
+            role = request.form.get("role")
+
+            user = User(email=email, firstname=firstname, lastname=lastname, password=generate_password_hash(password), role=role)
+            db.session.add(user)
+            db.session.commit()
+            flash("User updated!", category="success")
+            return redirect(url_for("views.admin"))
+
+    return render_template("update.html", user = update_user)
+
+@views.route("/<int:id>/delete", methods=["GET","POST"])
+def delete(id):
+    delete_user = User.query.filter_by(id = id).first()
+    if request.method == "POST":
+        if delete_user:
+            db.session.delete(delete_user)
+            db.session.commit()
+            return redirect(url_for("views.admin"))
+        abort(404)
+
+    return render_template("delete.html")
