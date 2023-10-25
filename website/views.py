16c24f99fdd9fa1e429406bf6a9a217ac1f3ce38
Lungelo <Lungelomkhwemte11@gmail.com>
2023-10-25 11:04:59 +0200

    UI updates and routes

--- website/views.py
+++ website/views.py
@@ -10,7 +10,7 @@ views = Blueprint("views", __name__)
 def home():
     return render_template("home.html")
 
-@views.route("/admin")
+@views.route("/admin", methods=["GET","POST"])
 def admin():
     users = User.query.all()
     return render_template("admin.html", users = users)
@@ -38,7 +38,7 @@ def update(id):
 
     return render_template("update.html", user = update_user)
 
-@views.route("/<int:id>/delete", methods=["GET","POST"])
+@views.route("/<int:id>/delete")
 def delete(id):
     delete_user = User.query.filter_by(id = id).first()
     if request.method == "POST":
@@ -48,4 +48,4 @@ def delete(id):
             return redirect(url_for("views.admin"))
         abort(404)
 
-    return render_template("delete.html")
+    
