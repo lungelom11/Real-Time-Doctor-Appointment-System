304ea5a6c1227a36e0b1f32b402d52d5b7bdd24b
Lungelo <Lungelomkhwemte11@gmail.com>
2023-10-26 15:23:50 +0200

    Added admin funtionalities (CRUD)

--- website/views.py
+++ website/views.py
@@ -1,19 +1,29 @@
-from flask import Blueprint, render_template, request,redirect, url_for,flash
-from flask_login import login_required, current_user
+from flask import Blueprint, render_template, request,redirect, url_for, flash, session
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash
+from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
+
 
 @views.route("/")
 def home():
     return render_template("home.html")
 
+
+
+@views.route("/book", methods=["GET","POST"])
+@login_required
+def book():
+    return render_template("book.html", user=current_user)
+
 @views.route("/admin", methods=["GET","POST"])
+@login_required
 def admin():
     users = User.query.all()
     return render_template("admin.html", users = users)
+
 
 
 @views.route("/<int:id>/update", methods=["GET","POST"])
@@ -38,14 +48,12 @@ def update(id):
 
     return render_template("update.html", user = update_user)
 
-@views.route("/<int:id>/delete")
+@views.route("/delete/<int:id>")
 def delete(id):
-    delete_user = User.query.filter_by(id = id).first()
-    if request.method == "POST":
-        if delete_user:
-            db.session.delete(delete_user)
-            db.session.commit()
-            return redirect(url_for("views.admin"))
-        abort(404)
+    delete_user = User.query.get_or_404(id)
+    db.session.delete(delete_user)
+    db.session.commit()
+    flash("User Deleted", category="success")
+    return redirect(url_for("views.admin"))
 
     
