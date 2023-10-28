3eed39451c850dabd69fe23a6b18aabb89b0ea60
Lungelo <Lungelomkhwemte11@gmail.com>
2023-10-28 19:32:09 +0200

    Make appointment UI

--- website/models.py
+++ website/models.py
@@ -8,3 +8,10 @@ class User(db.Model,UserMixin):
     lastname = db.Column(db.String(150))
     password = db.Column(db.String(150))
     role = db.Column(db.String(50))
+
+    def __init__(self, email, firstname, lastname, password, role):
+        self.email = email
+        self.firstname = firstname
+        self.lastname = lastname
+        self.password = password
+        self.role = role
